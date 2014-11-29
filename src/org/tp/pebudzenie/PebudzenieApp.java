f5937d269b442fabe548b2a7c2ec0ae26e68a427
Tomasz Trębski <ttrebski@ptc.com>
2014-11-29 20:41:32 +0100

    Initial activity

---
+++ src/org/tp/pebudzenie/PebudzenieApp.java
@@ -0,0 +1,13 @@
+package org.tp.pebudzenie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class PebudzenieApp
+        extends Activity {
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
